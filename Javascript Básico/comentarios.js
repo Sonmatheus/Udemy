0716229c5d2c0401972c01c0ea061e9b802902fa
Matheus Torres <132587437+Sonmatheus@users.noreply.github.com>
2026-06-23 16:28:55 -0300

    Comentarios

--- Javascript Básico/comentarios.js
+++ Javascript Básico/comentarios.js
@@ -1,1 +1,10 @@
 // E escrever um comentário
+console.log ("Olá MUNDO !") ; //Aqui tem outro comentário
+console.log(`Linha 1`);
+//COMENTÁRIO
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+ //Vai ser ignorado
+
+ /* To feliz 
