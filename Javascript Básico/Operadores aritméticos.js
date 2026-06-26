8213656dbd427cf92572b3085196e49ee6d6ae30
Matheus Torres <132587437+Sonmatheus@users.noreply.github.com>
2026-06-26 17:58:10 -0300

    Valores aritmëticos

---
+++ Javascript Básico/Operadores aritméticos.js
@@ -0,0 +1,12 @@
+/**
+ * Aritiméticos
+ * + Adição / Concatenação (-/*)
+ *  ** potenciação
+ * % Resto da divisão
+ */
+
+const num1 = 5
+const num2 =  2
+const num3 = 10
+
+console.log (num1 + num3 * num2)
---
+++ Javascript Básico/Valores primitivos.js
@@ -0,0 +1,8 @@
+const nome='Matheus' // string
+const nome1='Matheus'//string
+const nome2='Matheus'//string
+const num1='10'// number
+const num2='10.52'//number
+let nomealuno; //undefined -> não aponta pra local nenhum na memória 
+const sobrenomealuno= null // Nulo -> não aponta pra local nenhuma na memória
+const aprovado = false ; //boolean = true,false (Lógico)
