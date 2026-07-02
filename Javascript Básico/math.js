4581d5cdf0b9842fbc2bca96f5a4d87cf76adec8
Matheus Torres <132587437+Sonmatheus@users.noreply.github.com>
2026-07-01 21:08:31 -0300

    aulas de hj

---
+++ Javascript Básico/math.js
@@ -0,0 +1,10 @@
+let num1 = 9.221234876
+
+//let num2 = Math.floor(num1) //arredonda pra baixo o número
+//let num2= Math.ceil(num1) // arredonda para cima o número
+//let num2= Math.round(num1) //arredonda pro numero mais próximo
+//console.log (Math.max(1,2,4,15,787,-2222,3,9,120)) // Escolhe o num máximo na lista
+//console.log(Math.min(1,2,4,15,787,-2222,3,9,120))// Escolhe o num minímo na lista 
+//const aleatorio=Math.random() *  (10-5) //Número aleatorio
+
+console.log(aleatorio)
